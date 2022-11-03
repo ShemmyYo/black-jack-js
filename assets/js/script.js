476681d5519a6f748860ffe004b52ddb922785fb
ShemmyYo <rabiliamemo@gmail.com>
2022-11-03 16:53:46 +0000

    linked sum and message to inner HTML elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,15 +10,24 @@ let isAlive = true;
 
 let message = ""
 
+let messageEl = document.getElementById("message-el").innerHTML;
+
+
 function startGame() {
     
     if (sum <= 20) {
         message = "Do you want to draw another card?"
+        messageEl = message;
+
     } else if (sum === 21) {
         message = "Congratulations!!! You've got a Blackjack!"
+        messageEl = "Congratulations!!! You've got a Blackjack!"
+
         hasBlackJack = true;
     } else {
         message = "You loose!"
+        messageEl = "You loose!"
+
         isAlive = false;
     }
 console.log(message);
