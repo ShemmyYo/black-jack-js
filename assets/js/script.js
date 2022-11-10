90a5b559c02789a0e7425aec9bbaac8d6aa2cc1d
ShemmyYo <rabiliamemo@gmail.com>
2022-11-10 10:18:51 +0000

    added code to create/clear user to/from local storage. Linked to game.html - Comleted welcome page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,49 @@
-document.getElementById("bttn-card").style.display = "none";
-document.getElementById("change-player").style.display = "none";
-document.getElementById("enter-game").style.display = "none";
+// document.getElementById("bttn-card").style.display = "none";
+// document.getElementById("change-player").style.display = "none";
+// document.getElementById("enter-game").style.display = "none";
 
+// WELCOME Page DOM Elements
+let loginInputPlayerEl = document.getElementById("input-player-name");
+let loginBttnSaveNewPlayerEl = document.getElementById("bttn-save-player-name");
+let loginMsgBoxEl = document.getElementById("enter-game-msg");
+let loginBttnChangePlayerEl = document.getElementById("change-player");
+let loginBttnEnterGameEl = document.getElementById("enter-game");
 
-let player = {
-    name: "Shemmy",
-    credit: 3
-};
+let loginNewPlayerBoxEl = document.getElementById("player-new-name-empty");
+let loginOldPlayerBoxEl = document.getElementById("player-old-name-empty");
+
+// GAME Page DOM Elements
+let dealerSumEl = document.getElementById("dealer-sum");
+let playerSumEl = document.getElementById("player-sum");
+let messageEl = document.getElementById("message-el");
+let cardsEl = document.getElementById("cards-el");
+let playerEl = document.getElementById("player-el");
+
+// Variables
 
 let round = 1;
 let credit = 3;
-let welcomeInputPlayerEl = document.getElementById("input-player-name");
-let welcomePlayerMsgEl = document.getElementById("enter-game-msg");
-let cards = [];
-let sum = 0;
-let hasBlackJack = false;
-let isActive = false;
-let message = "";
-let messageEl = document.getElementById("message-el");
-let sumEl = document.getElementById("sum-el"); //unused variables
-let cardsEl = document.getElementById("cards-el");
-let playerEl = document.getElementById("player-el");
-playerEl.innerHTML = player.name + " Credit: " + player.credit;
 
-
-let dealerSum = 0;
-let dealerSumEl = document.getElementById("dealer-sum");
-dealerSumEl.innerHTML = dealerSum;
-
-let playerSum = 0;
-let playerSumEl = document.getElementById("player-sum");
-playerSumEl.innerHTML = playerSum;
-
+let cards;
 let cardsDeck;
 let hidden;
 
-var canHit = true;
+let dealerSum = 0;
+let playerSum = 0;
+
+let canHit = true;
+let canStay = true;
+let canReset = false;
+let hasBlackJack = false;
+
+let message = "";
+
+// playerEl.innerHTML = player.name + " Credit: " + player.credit;
+// dealerSumEl.innerHTML = dealerSum;
+// playerSumEl.innerHTML = playerSum;
+
+// Event Listeners
+
 
 
 
@@ -46,59 +54,48 @@ window.onload = function() {
     
 };
 
-function clearUser() {
-    console.log("clear user");
-    localStorage.clear("bj-playerName");
-    welcomePlayerMsgEl.innerHTML = "Enter New Player";
-
-    document.getElementById("change-player").style.display = "none";
-    document.getElementById("enter-game").style.display = "none";
-    document.getElementById("input-player-name").style.display = "compact";
-    document.getElementById("bttn-save-player-name").style.display = "compact";
-}
-
-function addUser() {
-
-    console.log("add user");
-    if (!welcomeInputPlayerEl.value) {
-    } else {
-        localStorage.setItem("bj-playerName", welcomeInputPlayerEl.value);
-        welcomePlayerMsgEl.innerHTML = "Welcome! " + welcomeInputPlayerEl.value;
-
-        document.getElementById("change-player").style.display = "compact";
-        document.getElementById("enter-game").style.display = "compact";
-        document.getElementById("input-player-name").style.display = "none";
-        document.getElementById("bttn-save-player-name").style.display = "none";
-    }
-}
-
 function validatePlayer() {
 
     if (localStorage.getItem("bj-playerName")) {
         console.log("user exists");
 
+        loginMsgBoxEl.innerHTML = "Welcome back! " + localStorage.getItem("bj-playerName");
         message =  "Welcome back! " + localStorage.getItem("bj-playerName");
-        welcomePlayerMsgEl.innerHTML = "Welcome back! " + localStorage.getItem("bj-playerName");
 
-        document.getElementById("change-player").style.display = "compact";
-        document.getElementById("enter-game").style.display = "compact";
-        document.getElementById("input-player-name").style.display = "none";
-        document.getElementById("bttn-save-player-name").style.display = "none";
+        loginNewPlayerBoxEl.style.display = "none";
+        loginOldPlayerBoxEl.style.display = "inline";
+  
 
     } else {
-        addUser();
+        addPlayer();
+   }
+}
+
+loginBttnChangePlayerEl.addEventListener("click", function remPlayer(){
+    console.log("clear user");
+    localStorage.clear("bj-playerName");
+
+    loginNewPlayerBoxEl.style.display = "inline";
+    loginOldPlayerBoxEl.style.display = "none";
+});
+
+
+loginBttnSaveNewPlayerEl.addEventListener("click", function addPlayer(){
+    console.log("add user");
+    if (!loginInputPlayerEl.value) {
+        alert("Enter and save Your name Player! ");
+    } else {
+        localStorage.setItem("bj-playerName", loginInputPlayerEl.value);
+        loginMsgBoxEl.innerHTML = "Welcome! " + localStorage.getItem("bj-playerName");
+
+        loginNewPlayerBoxEl.style.display = "none";
+        loginOldPlayerBoxEl.style.display = "inline";
     }
+});
 
-    messageEl.innerHTML = message;
-    welcomePlayerMsgEl.innerHTML = enterPlayerMsg;
-}
+loginBttnEnterGameEl.addEventListener("click", )
 
 
-function restartGame() {
-    createDeck();
-    shuffleDeck();
-    // startGame();
-}
 
 function createDeck() {
     let colors = ["H", "C", "D", "S"];
@@ -123,204 +120,6 @@ function shuffleDeck() {
     console.log(cardsDeck);
     }
 
-// function restartGame() {
-//     location.reload(true);  
-// }
-
-function restartGame_() {
-    console.log("restart function start");
-    cardsDeck = [];
-    console.log(cardsDeck);
-    playerSum = 0;
-    console.log(playerSum);
-    dealerSum = 0;
-    console.log(dealerSum);
-
-    let dealerCards = document.getElementById("dealer-cards");
-    while (dealerCards.firstChild) {
-        dealerCards.removeChild(dealerCards.firstChild);
-        }
-        console.log(dealerCards);
-
-    let playerCards = document.getElementById("player-cards");
-    while (playerCards.firstChild) {
-        playerCards.removeChild(playerCards.firstChild);
-        } 
-        console.log(playerCards); 
-
-    let cardImg = document.createElement("img");
-    let card = cardsDeck.pop();
-    document.getElementById("dealer-cards").append(cardImg);
-    cardImg.src = "./assets/images/deck/back.png";
-
-    dealerSumEl.innerHTML = "";
-    playerSumEl.innerHTML = playerSum;
-    messageEl.innerHTML = "";
-    playerEl.innerHTML = "Credit: " + credit; 
-    
-    canHit = true;
-    createDeck();
-    shuffleDeck();
-    startGame();
-    console.log("restart function stop");
-}
-
-function startGame() {
-    canHit = true;
-    hidden = cardsDeck.pop();
-    dealerSum += getValue(hidden);
-    console.log("hidden: " + hidden);
-    // console.log(dealerSum);
-
-    while (dealerSum < 17 ) {
-        let cardImg = document.createElement("img");
-        let card = cardsDeck.pop();
-        cardImg.src = "./assets/images/deck/" + card + ".png";
-        dealerSum += getValue(card);
-        document.getElementById("dealer-cards").append(cardImg);
-    }
-    console.log("D: " + dealerSum);
-
-    for (let i = 0; i < 2; i++) {
-        let cardImg = document.createElement("img");
-        let card = cardsDeck.pop();
-        cardImg.src = "./assets/images/deck/" + card + ".png";
-        playerSum += getValue(card);
-        document.getElementById("player-cards").append(cardImg);
-    }
-    console.log("P: " + playerSum);
-    document.getElementById("hit").addEventListener("click", hit);
-    document.getElementById("stay").addEventListener("click", stay);
-
-    playerSumEl.innerHTML = playerSum;
-}
-
-function hit() {
-    if (!canHit) {
-        return;
-    }
-    let cardImg = document.createElement("img");
-    let card = cardsDeck.pop();
-    cardImg.src = "./assets/images/deck/" + card + ".png";
-    playerSum += getValue(card);
-    document.getElementById("player-cards").append(cardImg);
-
-    if (playerSum <= 20) {
-        message = "Do you want to draw another card?";
-    } else if (playerSum === 21) {
-        message = "Congratulations!!! You've got a Blackjack!";
-        hasBlackJack = true;
-        canHit = false;        
-    } else {
-        message = "";
-        canHit = false;
-        stay();
-    }
-    messageEl.innerHTML = message;
-
-    playerSumEl.innerHTML = playerSum;
-}
-
-function stay() {
-    canHit = false;
-
-    if (playerSum === 21) {
-        credit += 5;
-        message = "Congratulations!!! You've got a Blackjack!<br>Your Credit has gone up by 5 and is now: " + credit;
-        
-    } else if (playerSum > 21) {
-        credit -= 1;
-        message = "You Lose " + player.name + "!<br>Your Credit has gone down by 1 and is now: " + credit;
-        
-    } else if (dealerSum > 21) {
-        credit +=3 ;
-        message = "You Win " + player.name + "!<br>Your Credit has gone up by 3 and is now:" + credit;
-
-    } else if (playerSum == dealerSum) {
-        message = "Tie!<br>Your Credit has not changeda and is now:" + credit;
-        
-    } else if (playerSum > dealerSum) {
-        credit += 1;
-        message = "You Win " + player.name + "!<br>Your Credit has not changeda and is now:" + credit;
-        
-    } else if (playerSum < dealerSum){
-        message = "You Lose " + player.name + "";
-    }
-    messageEl.innerHTML = message;
-    playerEl.innerHTML = "Credit: " + credit; 
-
-    dealerSumEl.innerHTML = dealerSum;
-    playerSumEl.innerHTML = playerSum;
-
-    console.log("before: " + cardsDeck);
-    console.log(hidden);
-    document.getElementById("hidden").src = "./assets/images/deck/" + hidden + ".png";
-    console.log("after: " + cardsDeck);
-
-}
-
-function getValue(card) {
-    let data = card.split("-");
-    let value = data[0];
-
-    if (isNaN(value)) {
-        if (value == "A") {
-            return 11;
-        } 
-        return 10;
-    }
-    return parseInt(value);
-}
-
-
-
-
-
-
-
-
-
-// function startGame2() {
-//         if (sum === 0) {
-//         isActive = true;
-//         hasBlackJack = false;
-//         let card1 = getRandomCard();
-//         let card2 = getRandomCard();
-//         cards = [card1, card2];
-//         sum = card1 + card2;
-
-//         game();
-
-//         // document.getElementById("bttn-start").style.display = "hidden";
-//         // document.getElementById("bttn-card").style.display = "visible";
-//         // document.getElementById("bttn-save").style.display = "visible";
-//     }
-// }
-
-
-
-
-
-
-function slideWelcomeWindowLeft() {
-
-    let id = null;
-    const welcomeWindow = document.getElementById("instructions-wraper") 
-    let pos = 0;
-    let opa = 1;
-    clearInterval(id);
-    id = setInterval(frame, 0);
-    function frame() {
-        if (pos == -1000) {
-            clearInterval(id);
-        } else {
-            pos++; 
-            opa = opa - 0.009;
-            // welcomeWindow.style.top = pos + "px"; 
-            welcomeWindow.style.right = pos + "px"; 
-            welcomeWindow.style.opacity = opa;
-        }
-    }
-
-
+function restartGame() {
+    location.reload(true);  
 }
